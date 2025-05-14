691d1808c6d491c016cead93f3c6e7d68d59371a
Oliver <oliver_tu@brown.edu>
2025-05-14 03:08:01 -0400

    cleaned up frontend files + updated readme

--- mock/src/components/select/App.tsx
+++ mock/src/components/select/App.tsx
@@ -1,27 +1,19 @@
 import { useState } from "react";
 import "@/styles/App.css";
-import { LoginButton } from "../LoginButton";
 import { CourseSearchPage } from "./CourseSearchPage";
 
 /**
  * This is the highest level of the Course Recommendation Application
  */
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
   return (
     <div className="App">
       <div className="App-header">
-        <h1 aria-label="Course Recommendation Application">Brown Course Finder</h1>
-        <LoginButton isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <h1 aria-label="Course Recommendation Application">
+          Brown Course Finder
+        </h1>
       </div>
-      {isLoggedIn ? (
-        <CourseSearchPage />
-      ) : (
-        <div id="modal-root" aria-label="Please log in to continue">
-          Please log in to continue
-        </div>
-      )}
+      <CourseSearchPage />
     </div>
   );
 }
